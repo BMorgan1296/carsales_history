6a22c69332ce6a29055657cccc56ed100cf722f7
BMorgan1296 <33080621+BMorgan1296@users.noreply.github.com>
2021-08-19 01:21:43 +0930

    Added lots of functionality
    Currently can query and save multiple searches. These are saved as pickle objects in saved_searches/.
    
    Currently in testing mode, does not query the website, uses output1.txt for HTML parsing
    
    update() for listings class is currently not working, does not add multiple prices or times. Also adds new info even if price hasn't changed, this needs to be fixed.
    
    Need to implement a way to check if a car still exists or has been sold or is unavailable. Could loop through objects not found in most recent search, which have not been marked as sold or unavailable in the script. This should do the trick.

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-pids=$(pgrep Xvfb)
+pids=$(pgrep "Xvfb")
 kill $pids >/dev/null 2>&1
 
-pids=$(pgrep chrom)
+pids=$(pgrep "chrom")
+kill $pids >/dev/null 2>&1
+
+pids=$(pgrep "python")
 kill $pids >/dev/null 2>&1
 
 python3 selenium_test.py
--- selenium_test.py
+++ selenium_test.py
@@ -1,38 +1,35 @@
 #https://stackoverflow.com/questions/64979042/how-to-run-seleniumchrome-on-raspberry-pi-4
 #https://stackoverflow.com/questions/35956045/extract-title-with-beautifulsoup
 
+import os
+import sys
+import time
+import random
+import pickle
+import requests
+
+from pyvirtualdisplay import Display
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 from bs4 import BeautifulSoup, NavigableString, Tag
 
-from pyvirtualdisplay import Display
-
-import atexit
-import sys
-import time
-import random
-  
 sys.path.insert(0, './bypass_geetest_slider')
-
-#from bypass_geetest_slider.captcha_solver.nocaptcha import CapatchaSolver
-
-driver = None
+from bypass_geetest_slider.captcha_solver.nocaptcha import CapatchaSolver
 
 def log(logstr):
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     print("%s: %s" % (current_time, logstr))
 
-
-
-    #https://www.carsales.com.au/cars/used/nissan/south-australia-state/adelaide-region/para-hills-5096-suburb/manual-transmission/turbo-induction/under-3000/over-10000km-kilometres/over-1500cc-engine-size/over-80kw-power/over-1500kg-towing-brakes/
 class carsales_scraper:
-    def __init__(self):
-        log("Beginning display setup")
+    def __init__(self, timeout):
+        self.timeout = timeout
+        log("Beginning virtual display setup")
         self.display = Display(visible=0, size=(1920, 1080))
         self.display.start()
-        log("Display started, setting Driver options")
+        log("Virtual Display started, setting ChromeDriver options")
 
         self.chrome_options = Options()
         self.chrome_options.add_argument("--gpu_compositing")
@@ -45,68 +42,214 @@ class carsales_scraper:
         self.chrome_options.add_argument("--log-level=3")
         self.chrome_options.add_argument("--allow-running-insecure-content");
 
-        log("Starting Driver")
+        log("Starting ChromeDriver")
         self.driver = webdriver.Chrome(options=self.chrome_options, executable_path='/usr/lib/chromium-browser/chromedriver')
+        log("ChromeDriver successfully setup")
 
     def load_page(self, url):
         website = None
         loading_finished = 0                   # Set flag to 0
+        count = 0
         while loading_finished == 0:           # Repeat while flag = 0
             try:
-                self.driver.set_page_load_timeout(45)
+                self.driver.set_page_load_timeout(self.timeout)
                 time.sleep(random.uniform(0.1, 0.5)) # wait some time
-                loading_finished = 1            # Set flag to 1 and exit while loop
+                self.driver.get(url)       # try to load for n seconds
+                loading_finished = 1                 # Set flag to 1 and exit while loop
+            except KeyboardInterrupt:
+                log("SIGINT Ctrl-C - Exiting")
+                self.kill_driver()
+                sys.exit(1)
             except:
                 log("Timeout - retry")
+            count += 1
+            if(count == 25):
+                log("Too many tries (25) - Exiting")
+                self.kill_driver()
+                sys.exit(1)
         else:
             log("Page loaded")
-
-    def get_curr_webpage(self):
-        website = self.driver.get(url)       # try to load for n seconds
-        return website
+        return self.driver.page_source
 
     def test_captcha_exist(self, basepage):
-        self.load_page(basepage)
+        log("Checking CarSales for CAPTCHA")
+        website = self.load_page(basepage)
         try:
             geetest = driver.find_element_by_class_name("geetest_radar_tip")
             print(geetest)
-            log("Geetest captcha found")
+            log("Geetest CAPTCHA found")
+            return 1
             # captcha_solver = CapatchaSolver(self.driver)
         except Exception as e:
-            log("No Geetest captcha found")
-
-    def get_curr_listings(self, html):
-        print("TODO")
+            log("No Geetest CAPTCHA found")
+            return 0
 
     def kill_driver(self):
         self.driver.close()
         self.driver.quit()
 
-class car_advert():
-    def __init__(self, id):
-        self.id = id
-        #self.last_updated = time.now()
+#Handles listing and updated data
+class listing:
+    def __init__(self, idx, title, ad_type, bodystyle, make, model, ad_state, price, picture_url):
+        #Untracked data
+        self.id = idx
+        self.title = title
+        self.ad_type = ad_type
+        self.bodystyle = bodystyle
+        self.make = make
+        self.model = model
+        self.ad_state = ad_state
+        self.picture_url = picture_url
+        self.url = "https://www.carsales.com.au/cars/details/x/"+self.id
+
+        #Tracked data (will be one or more of these pieces of info)
+        self.tracked_data = []
+        #Price and time of query
+        self.tracked_data.append((price, time.time()))
+
+        self.picture = requests.get(picture_url)
+
+        #0 sold, 1 alive. 2 on hold (need to find how to differentiate this)
+        # Init as alive.
+        self.status = 1
+
+    def update(self, price):
+        self.tracked_data.append((price, time.time()))
+
+    def print_listing(self):
+        print(self.title)
+        print(self.id, self.ad_type, self.ad_state)
+        print(self.bodystyle, self.make, self.model)
+        print(self.url)
+        print(self.picture_url)
+        for p in self.tracked_data:
+            print(p)
+
+class saved_search:
+    def __init__(self, name, search_url):
+        self.name = name
+        self.search_url = search_url
+        self.listings = []
+        self.last_updated = -1       
+
+    def __add_listing(self, idx, title, ad_type, bodystyle, make, model, ad_state, price, picture_url):
+        #Go through all listings, if ID found then update. Otherwise, add a new listing.
+        for l in self.listings:
+            if(l.id == idx):
+                l.update(ad_type, price)
+                return
+        self.listings.append(listing(idx, title, ad_type, bodystyle, make, model, ad_state, price, picture_url))
+
+    def __update_from_page(self, website):
+        title = ""
+        picture_url = ""
+        page = BeautifulSoup(website, 'html.parser')
+        listings = page.find_all("div", class_="listing-items")
+        listings = listings[0].find_all('div', class_="listing-item", recursive=False)
+        for listing in listings:
+            #Get image url. Will save this at some point.
+            for img in listing.find_all('img', recursive=True):
+                try:
+                    picture_url = img.attrs['src']
+                except:
+                    picture_url = "Picture unavailable"
+                break
+            #Get title
+            for card in listing.find_all('div', class_="card-body", recursive=True):
+                for inner in card.find_all('a', class_="js-encode-search", recursive=True): 
+                    try:
+                        title = inner.decode_contents()
+                    except:
+                        title = "Title unavailable"
+                    break
+            #Add the new listing, or update an existing one if this is found.
+            try:
+                self.__add_listing(
+                    listing.attrs['id'], 
+                    title,
+                    listing.attrs['data-webm-vehcategory'],
+                    listing.attrs['data-webm-bodystyle'],
+                    listing.attrs['data-webm-make'],
+                    listing.attrs['data-webm-model'],
+                    listing.attrs['data-webm-state'],
+                    listing.attrs['data-webm-price'],
+                    picture_url)
+            except Exception as e:
+                log("Error parsing ID %s" % listing.attrs['id'])
+                return -1
+        return len(listings)
+
+    def update(self, skip):
+        log("Starting update for \"%s\"" % self.name)
+        self.last_updated = time.time()
+        d = carsales_scraper(45)
+        time.sleep(3)
+        #get first page
+        if(skip or d.test_captcha_exist("https://www.carsales.com.au/cars/") == 0):
+            #loop through all listings, see if any haven't been updated. If so, mark as sold or unavailable.
+            page = 0
+            num = 1
+            while(num):
+                url = self.search_url + "&offset=" + str(12*page)
+                log("Querying: "+url)
+                #start parsing
+                #website = d.load_page(url)
+                website = open("output1.txt", "r")
+                num = self.__update_from_page(website)
+                print(num)
+                for l in self.listings:
+                    l.print_listing()
+                    print("")
+                website.close()
+                ###########BREAK EARLY################
+                break
+                page += 1
+                #get next page
+                #TODO
+        #Check if any listing weren't updated after the last_updated time
+        d.kill_driver()
+
+
 
 def main():
-    url = "https://www.carsales.com.au/cars/"
-    url2 = "https://www.carsales.com.au/cars/?q=(And.Service.carsales._.Make.Nissan._.Drive.Rear+Wheel+Drive._.GenericGearType.Manual._.Induction.Turbo._.Year.range(..2001).)&sort=%7ePrice"
+    base_url = "https://www.carsales.com.au/cars/"
+    search_url = "https://www.carsales.com.au/cars/?q=(And.Service.carsales._.Make.Nissan._.Drive.Rear+Wheel+Drive._.GenericGearType.Manual._.Induction.Turbo._.Year.range(..2001).)&sort=%7ePrice"
+    search_url2 = "https://www.carsales.com.au/cars/nissan/skyline/coupe-bodystyle/manual-transmission/turbo-induction/6-cylinders/"
 
-    #d = carsales_scraper()
-    
-    # log("Driver setup, querying CarSales")
-    # time.sleep(3)
-    # d.test_captcha_exist(url)
+    searches_dir = "./saved_searches/"
+    searches = []
 
-    # log("Querying Carsales for given URL")
-    # d.load_page(url2)
-    # page = d.get_curr_webpage()
+    #load from pickle
+    for file in os.listdir(searches_dir):
+        if file.endswith("obj"):
+            try:
+                log("Found existing search \"%s\" - Loading" % file)
+                file_pi = open(searches_dir+file, 'rb') 
+                searches.append(pickle.load(file_pi))
+                file_pi.close()
+                log("Load successful")
+            except:
+                log("Failed to load \"%s\" - Exiting" % file)
+                sys.exit(1)
 
-    f = open("output1.txt", "r")
-    page = BeautifulSoup(f, 'html.parser')
-    listings = page.find_all("div", class_="listing-items")
-    for listing in listings[0].find_all('div', class_="listing-item", recursive=False):
-        print(listing.attrs['id'])
-    f.close()
+    found = 0
+    temp = saved_search("Nissan Turbo Search", search_url)
+    for s in searches:
+        if(s.name == temp.name):
+            log("Search already exists - Continuing")
+            found = 1
+
+    if(found == 0):
+        searches.append(temp)
+
+    for s in searches:
+        s.update(1)
+        log("Update successful - Saving")
+        file_pi = open(searches_dir+s.name+'.obj', 'wb') 
+        pickle.dump(s, file_pi)
+        log("Saved")
+
+    log("All searches updated and saved, exiting")
     
 if __name__ == "__main__":
     main()
